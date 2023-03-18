ce0374d99e537287ba1c2c21d0022c3bce878bed
jamiecartin <jamie.cartin@icloud.com>
2023-03-18 05:43:30 -0400

    Initial Commit

---
+++ MovieBooking/MovieBookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieBookingApp.swift
+//  MovieBooking
+//
+//  Created by Jamie Cartin on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct MovieBookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
